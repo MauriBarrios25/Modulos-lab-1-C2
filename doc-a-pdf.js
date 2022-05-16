0936b85d795c3c163a936a71e045fa3fbbb152df
Mauricio Montiel <smis952220@ugb.edu.sv>
2022-05-16 00:10:14 -0600

    cambios

--- doc-a-pdf.js
+++ doc-a-pdf.js
@@ -1,12 +1,28 @@
 var docx = require("docx-pdf")
+var readline = require("readline")
 
-docx("./docx/docx1.docx" , "./pdf/docx1-pdf.pdf" , function(err,result){
-    if (err) {
-        
-        console.log(err);
-        
-    }
-    console.log("result" + result);
+var rl = readline.createInterface(
+    process.stdin, process.stdout);
 
+// En este caso deje un documento por defecto llamado docx1.docx pero solo basta con escribir docx1, se pueden colocar mas documentos y solo se debe especificar nombre
+function solicitar() {
+    rl.question('Ingrese el nombre del documento sin extension: ' , (namedoc) => {
+        convertirpdf(namedoc)
+    });
 
-})
+}
+
+function convertirpdf(nodoc) {
+    docx("./docx/" + nodoc + ".docx"  , "./pdf/" + nodoc + "-pdf" + ".pdf" , function(err,result){
+        if (err) {
+            
+            console.log(err);
+            
+        }
+        console.log("result" + result);
+    
+    
+    }) 
+}
+
+solicitar();
